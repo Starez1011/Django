99658bf6452129c93bb34c243f9ef5489cb43dba
Sameer Maharjan <103033131+Starez1011@users.noreply.github.com>
2024-06-09 15:47:02 +0545

    Delete chat directory

--- chat/chatbot/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Chat
-# Register your models here.
-@admin.register(Chat)
-class ChatAdmin(admin.ModelAdmin):
-    list_display = ['id','user','message','response','created_at']
